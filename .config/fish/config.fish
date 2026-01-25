de0b6e72cfae98ae38547fa3df36a935f92d5b34
hammerlink <hendrik.hamerlinck@hammernet.be>
2026-01-25 21:30:22 +0100

    configure fish and starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
+    atuin init fish | source
+    starship init fish | source
+    bind \cl accept-autosuggestion
 end
