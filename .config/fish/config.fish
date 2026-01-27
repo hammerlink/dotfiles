09825bca977b72d7117892116c451723b6549d8c
hammerlink <hendrik.hamerlinck@hammernet.be>
2026-01-27 09:08:29 +0100

    remove unused fish keymap

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     atuin init fish | source
     # starship init fish | source
-    bind \cl accept-autosuggestion
 
     set -gx PATH $HOME/.cargo/bin $PATH
 
