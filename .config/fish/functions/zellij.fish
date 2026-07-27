6798b1f0d5826d662d045d16bcc0ac4896b8484c
hammerlink <hendrik.hamerlinck@hammernet.be>
2026-07-27 21:39:14 +0200

    use hostnamectl instead of hostname

--- .config/fish/functions/zellij.fish
+++ .config/fish/functions/zellij.fish
@@ -1,6 +1,6 @@
 function zellij
     if not contains -- -s $argv; and not contains -- --session $argv
-        command zellij attach -c (hostname) $argv
+        command zellij attach -c (hostnamectl hostname) $argv
     else
         command zellij $argv
     end
