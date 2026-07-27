3dbd4aec0cbdbe81dbe83b6eee8352d33ad0c72e
hammerlink <hendrik.hamerlinck@hammernet.be>
2026-07-27 21:33:28 +0200

    improve the zellij default session

--- .config/fish/functions/zellij.fish
+++ .config/fish/functions/zellij.fish
@@ -1,6 +1,6 @@
 function zellij
     if not contains -- -s $argv; and not contains -- --session $argv
-        command zellij --session (hostname) $argv
+        command zellij attach -c (hostname) $argv
     else
         command zellij $argv
     end
